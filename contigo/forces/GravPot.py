a55b0c8d9f9b1c1d8428ab73e8e74faf588ac40f
Kyle Murphy <murphy.kyle.r@gmail.com>
2026-02-05 08:27:09 -0500

    Update GravPot.py
    Change this to use km's rather then m's.
    
    km's seem to be what's use in orbits and distance

--- contigo/forces/GravPot.py
+++ contigo/forces/GravPot.py
@@ -14,7 +14,7 @@ from .grav_utils import get_potential
 class GravPot():
     """Class to derive gravatational potential for a set of ECEF coordinates.
     """
-    def __init__(self, r: npt.ArrayLike=np.array(6771000.0,ndmin=1),
+    def __init__(self, r: npt.ArrayLike=np.array(6771.0,ndmin=1),
                  lat: npt.ArrayLike=np.array(0,ndmin=1),
                  lon: npt.ArrayLike=np.array(np.pi,ndmin=1),
                  pot_file: str = 'EIGEN-2.gfc',
@@ -24,12 +24,12 @@ class GravPot():
         Position (r, lat, lon) needs to be in an Earth Centered Earth Fixed
         (ECEF) coordinate frame.     
         Units are m and radians for position (r, lat, lon).
-        The Gravatational Potential is calculated in units of m^2/s^2.
+        The Gravatational Potential is calculated in units of km^2/s^2.
 
         Parameters
         ----------
         r : npt.ArrayLike, optional
-            An array of radial positions (ECEF - meters), 
+            An array of radial positions (ECEF - kilometers), 
             by default np.array(6771000.0,ndmin=1)
         lat : npt.ArrayLike, optional
             An array of latitude positions (ECEF - radians), 
@@ -55,11 +55,20 @@ class GravPot():
         lon = np.asarray(lon)
         if lon.ndim == 0: lon = lon.reshape(1)
 
+        # Initial Variables For Potential
         self.r = r
         self.lat = lat
         self.lon = lon
+        
         self.pot_file = pot_file
         self.lmax = lmax
+        self.gravpot = None
+
+        # Modeled Field Variables Which are Loaded
+        self.clm = None
+        self.slm = None
+        self.r0 = None
+        self.GM = None
 
         if os.path.isfile(pot_file):
             self.load_coeff()
@@ -79,8 +88,8 @@ class GravPot():
             self.pot_file = pot_file
 
         self.clm, self.slm, cs_meta = read_icgem_coeff(self.pot_file)
-        self.r0 = cs_meta['r0']
-        self.GM = cs_meta['GM']
+        self.r0 = cs_meta['r0']/1000. # convert to km
+        self.GM = cs_meta['GM']/(1000.**3) # convert to km^3/s^2
         # check the lmax of the potential file
         if not self.lmax:
             self.lmax = cs_meta['lmax']
@@ -89,7 +98,7 @@ class GravPot():
             print('Setting lmax to that in the potential file.')
             self.lmax = cs_meta['lmax']
 
-    def get_pot(self):
+    def calc_pot(self):
         """Derive Potential.
 
         Potential is derived using normalize Legendre Coefficients from
@@ -99,7 +108,7 @@ class GravPot():
         values which are then passed to .grav_utils._get_potential_numba_core
         which uses numba and jit to improve the performace of the calculation.
 
-        The Gravatational Potential is calculated in units of m^2/s^2.
+        The Gravatational Potential is calculated in units of km^2/s^2.
 
         Raises
         ------
@@ -108,14 +117,25 @@ class GravPot():
         ValueError
             If the size of the position values, r, lat, lon are not the same.
         """
-        # check to make r, lat, lon are the same size       
-        if not hasattr(self, 'clm') or not hasattr(self, 'slm'):
+        # check if coeffecients have been loaded
+        if self.clm is None:     
             raise ValueError('No coeffecients have been loaded, use load_coef( )')
-
+        # check to make r, lat, lon are the same size
         if len(self.r) != len(self.lat) or len(self.r) != len(self.lon):
             raise ValueError('r, lat, and lon must be same length')
 
         self.gravpot = np.array([get_potential(rr, rlat, rlon,
                     self.clm, self.slm, self.GM, self.r0, lmax=self.lmax)
                     for rr, rlat, rlon in zip(self.r,self.lat,self.lon)])
+
+    def get_pot(self):
+        """Return Potential.
+
+        Returns
+        -------
+        np.array
+            Array of potetial in km^2/s^2
+        """
+        if self.gravpot:
+            return self.gravpot
         
