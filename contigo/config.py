60c5022425b1d7d9eb90cb7a17a544e1e904a066
Kyle Murphy <murphy.kyle.r@gmail.com>
2026-02-19 06:04:40 -0500

    Create config.py
    config file for storing module configuration and state variables.
    
    Configuration variables include things like the data directory.
    
    State variables include things like weather we've attempted to download files or if certain files have been loaded already.
    
    This is meant to save overhead for download and loading and files.

---
+++ contigo/config.py
@@ -0,0 +1,28 @@
+"""Config and State values for CONTIGO
+
+added: 19/02/2026 Kyle Murphy <kylemurphy.spacephys@gmail.com>
+"""
+
+from os import makedirs
+from pathlib import Path
+
+
+def data_path():
+    # setup data dir 
+    # find directory of module
+    # module directory/swdata/ is where the data is stored
+    f_path = Path(__file__).resolve()
+    d_dir = f_path / '..' / 'data'
+    d_dir = d_dir.resolve()
+
+    # create it if it doesn't exist
+    if not d_dir.exists():
+        makedirs(d_dir)
+
+    return d_dir._str
+
+# Static configuration values (constants)
+DATA_DIR = data_path()
+
+# Mutable State Values
+state = {'kernel_downloaded':False}
