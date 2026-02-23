af86a289a3f6a378f1e84253de76e6b9b591824c
Kyle Murphy <murphy.kyle.r@gmail.com>
2026-02-23 15:53:47 -0500

    Setup and load gmat
    Store gmat in config state

--- contigo/forces/srp_utils.py
+++ contigo/forces/srp_utils.py
@@ -3,20 +3,32 @@
 added: 23/02/2026 Kyle Murphy <kylemurphy.spacephys@gmail.com>
 """
 import sys
-from os import path
+import logging
+import os
+from pathlib import Path
 
 import contigo.config as config
 
-def setup_gmat(apistartup, gmat_install):
+logger = logging.getLogger(__name__)
 
-    gmat_bin_path = gmat_install + "/bin"
-    startup = gmat_bin_path + "/" + apistartup
+def setup_gmat(apistartup: str | Path,
+               gmat_install: str | Path):
 
-    if path.exists(startup):
+    gmat_bin_path = os.path.join(gmat_install, "bin")
+    startup = os.path.join(gmat_bin_path, apistartup)
+
+    if os.path.exists(startup) and config.state['gmat_loaded'] is False:
+        logger.info('Setting up GMAT API.')
         sys.path.insert(1, gmat_bin_path)
         import gmatpy as gmat
         gmat.Setup(startup)
 
         config.state['gmat_loaded'] = True
+        config.state['gmatpy'] = gmat
+        
+    elif config.state['gmat_loaded']:
+        logger.info('GMAT API already setup.')
+        import gmatpy as gmat
+        config.state['gmatpy'] = gmat
     else:
         raise ValueError(f"Cannot find {startup} in {gmat_bin_path}")
