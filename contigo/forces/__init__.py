ec99744ebebc6724dbe02f6f426e8ce4198558b0
Kyle Murphy <murphy.kyle.r@gmail.com>
2026-02-26 14:27:14 -0500

    update inits

---
+++ contigo/forces/__init__.py
@@ -0,0 +1,4 @@
+"""Derive third body accelerations for an Earth orbiting spacecraft.
+
+added: 26/02/2026 Kyle Murphy <kylemurphy.spacephys@gmail.com>
+"""
---
+++ contigo/utils/__init__.py
@@ -0,0 +1,4 @@
+"""Derive third body accelerations for an Earth orbiting spacecraft.
+
+added: 18/02/2026 Kyle Murphy <kylemurphy.spacephys@gmail.com>
+"""
