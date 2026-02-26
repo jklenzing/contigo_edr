0c69e14725ac1b7a562a4d400467351f3d3ee630
Kyle Murphy <murphy.kyle.r@gmail.com>
2026-02-26 06:05:26 -0500

    Update solar_system_ephem.py
    Convert so that we are now using GPS time to qauntize rather then spice ephemeris time.

--- contigo/solar_system_ephem.py
+++ contigo/solar_system_ephem.py
@@ -160,7 +160,8 @@ class SolarSystemEnvironment:
     def __init__(
         self,
         bodies: npt.NDArray[np.str_],
-        et: npt.NDArray[np.float64],
+        sp_et: npt.NDArray[np.float64],
+        sp_gps: npt.NDArray[np.float64],
         tolerance: float | None,
         provider: SPICEEphem,
     ) -> None:
@@ -177,8 +178,9 @@ class SolarSystemEnvironment:
         # value -> (Nb, 3) position array
         self._cache: dict[float, np.ndarray] = {}
 
-        et = np.asarray(et, dtype=float)
-        self._load_times(et)
+        sp_et = np.asarray(sp_et, dtype=float)
+        sp_gps = np.asarray(sp_gps,dtype=float)
+        self._load_times(sp_et,sp_gps)
 
     # ----------------------------------------------------------
     # Public API
@@ -186,13 +188,15 @@ class SolarSystemEnvironment:
 
     def get_ephem(
         self,
-        et: npt.NDArray[np.float64],
+        sp_et: npt.NDArray[np.float64],
+        sp_gps: npt.NDArray[np.float64],
     ) -> tuple[np.ndarray, np.ndarray]:
 
-        et = np.asarray(et, dtype=float)
+        sp_et = np.asarray(sp_et, dtype=float)
+        sp_gps = np.asarray(sp_gps,dtype=float)
 
         # Ensure all times are loaded
-        self._load_times(et)
+        self._load_times(sp_et, sp_gps)
 
         #-----------
         # old method
@@ -210,11 +214,11 @@ class SolarSystemEnvironment:
         # new method
         # ~10x faster
         #-----------
-        key = self._quantize(et)
+        key = self._quantize(sp_gps)
         r_out = np.array(itemgetter(*key)(self._cache))
         r_out = np.swapaxes(r_out,0,1)
 
-        return et, r_out
+        return sp_et, sp_gps, r_out
 
     # ----------------------------------------------------------
     # Internal
@@ -229,30 +233,34 @@ class SolarSystemEnvironment:
             return t
         return np.round(t / self.tolerance).astype(int)
 
-    def _load_times(self, et: np.ndarray) -> None:
+    def _load_times(self, 
+                    sp_et: np.ndarray,
+                    sp_gps: np.ndarray,) -> None:
         """
         Load only times not already cached.
         """
 
         # Quantize requested times
-        q_times = self._quantize(et)
+        q_times = self._quantize(sp_gps)
 
         # Identify which quantized times are missing
-        missing = [i for i in q_times if i not in self._cache]
+        missing = [[i,t] for i, t in zip(q_times,sp_et) if i not in self._cache]
 
         if not missing:
             return
 
+        missing_qt, missing_et = zip(*missing)
+
         # convert the missing qauntized time back 
         # to normal et time
-        missing_et = np.array(missing, dtype=float)
-        if self.tolerance:
-            missing_et = np.array(missing, dtype=float)*self.tolerance
+        #missing_et = np.array(missing, dtype=float)
+        #if self.tolerance:
+        #    missing_et = np.array(missing, dtype=float)*self.tolerance
 
         # Call provider once for all missing times
         _, r_new = self._provider(self.bodies, missing_et)
 
         # Store per-time slice in dictionary
-        for i, t in enumerate(missing):
+        for i, t in enumerate(missing_qt):
             # r_new shape = (Nb, Nt_missing, 3)
             self._cache[t] = r_new[:, i, :]
